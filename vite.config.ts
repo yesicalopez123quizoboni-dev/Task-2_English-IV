f7921b68a43455c7fae3f0e77542c4cb81b4d030
yesicalopez123quizoboni-dev <yesicalopez123quizoboni@gmail.com>
2026-09-17 09:14:01 -0500

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
-  return {
+  return {base: '/Task-2_English-IV/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
